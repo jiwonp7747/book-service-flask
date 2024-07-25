b90f7e02a15d5314a51cb4d9a6cd872118adbc56
jiwon <angry9908@gmail.com>
2024-07-26 00:39:56 +0900

    generate_content 추가

--- gemini_api.py
+++ gemini_api.py
@@ -1,6 +1,5 @@
 import google.generativeai as genai
-
-GOOGLE_API_KEY = 'AIzaSyDRCu-IKmQRRX4WQ3Cmhzkl2oRyP0vO_I0'
+from api_key import GOOGLE_API_KEY
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
@@ -15,11 +14,21 @@ generation_config = {
 model = genai.GenerativeModel('gemini-pro',
                              generation_config=generation_config)
 
-prompt = """
-- 백엔드 개발 관련 책을 5권 추천해주고
-- 각 책의 저자와 간단한 설명을 추가해주세요.
+# prompt = """
+# - 백엔드 개발 관련 책을 5권 추천해주고
+# - 각 책의 저자와 간단한 설명을 추가해주세요.
 
-"""   
+# """   
 
-response = model.generate_content(prompt)
-print(response.text)
+def generate_content(book):
+  
+    prompt = f"""
+    - {book} 책에 대해서 설명해주세요.
+    - 단 50자 안으로 설명해주세요.
+    """   
+    generate_content_response = model.generate_content(prompt)
+    content=generate_content_response.text
+    
+    return content
+  
+  
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
 import boto3, json
+from gemini_api import generate_content
 from werkzeug.utils import secure_filename
 from socket import *
 
@@ -10,15 +11,21 @@ app = Flask(__name__)
 def receive_string():
 	#Spring으로부터 JSON 객체를 전달받음
     dto_json = request.get_json()
+    book_name=dto_json.get('bookName', None)
     
-    #dto_json을 dumps 메서드를 사용하여 response에 저장
-    response = json.dumps(dto_json, ensure_ascii=False)
-	
-   	#Spring에서 받은 데이터를 출력해서 확인
-    print(dto_json)
+    content =""
+    # nickname이 존재하면 generate_content 호출 
+    if book_name:
+        content = generate_content(book_name)
+    else:
+        content="닉네임이 존재하지 않습니다."
     
-    #Spring으로 response 전달
-    return response
+    response_data={"bookName":book_name, "content":content}
+    
+    # jsonify(response_data)
+    print(response_data)
+    
+    return json.dumps(response_data, ensure_ascii=False)
 
 # 0.0.0.0 으로 모든 IP에 대한 연결을 허용해놓고 포트는 8082로 설정
 if __name__ == '__main__':
